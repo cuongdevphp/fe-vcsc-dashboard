8cb0ba6641f905edc187f0ae1d0a694119384fa2
Cuong Au <cuongah@vcsc.com.vn>
2022-03-02 14:19:59 +0700

    c

--- src/app/dashboard/trading/trading.component.ts
+++ src/app/dashboard/trading/trading.component.ts
@@ -1,67 +1,136 @@
-import { Component, OnInit, TemplateRef, AfterViewInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NzMessageService } from 'ng-zorro-antd/message';
-import { NzModalService } from 'ng-zorro-antd/modal';
-import { NzTableQueryParams } from 'ng-zorro-antd/table';
-import { UsersService } from 'src/app/shared/services/users.service';
-
-declare const TradingView: any;
-
+import { Component, Input, OnDestroy, OnInit, TemplateRef } from '@angular/core';
+import { ChartingLibraryWidgetOptions, IChartingLibraryWidget, LanguageCode, ResolutionString, widget } from 'src/assets/charting_library/charting_library.min';
 @Component({
-    templateUrl: './trading.component.html'
+    templateUrl: './trading.component.html',
+    styleUrls: ['./trading.component.css']
 })
 
-export class TradingDashboardComponent implements OnInit, AfterViewInit {
-    ngOnInit() {
+export class TradingDashboardComponent implements OnInit, OnDestroy  {
+    private _symbol: ChartingLibraryWidgetOptions['symbol'] = 'VNM';
+    private _interval: ChartingLibraryWidgetOptions['interval'] = 'D' as ResolutionString;
+    // BEWARE: no trailing slash is expected in feed URL
+    private _datafeedUrl = 'https://invest.vcsc.com.vn/api/v2/tradingview';
+    private _libraryPath: ChartingLibraryWidgetOptions['library_path'] = '/assets/charting_library/';
+    private _chartsStorageUrl: ChartingLibraryWidgetOptions['charts_storage_url'] = 'https://saveload.tradingview.com';
+    private _chartsStorageApiVersion: ChartingLibraryWidgetOptions['charts_storage_api_version'] = '1.1';
+    private _clientId: ChartingLibraryWidgetOptions['client_id'] = 'tradingview.com';
+    private _userId: ChartingLibraryWidgetOptions['user_id'] = 'public_user_id';
+    private _fullscreen: ChartingLibraryWidgetOptions['fullscreen'] = false;
+    private _autosize: ChartingLibraryWidgetOptions['autosize'] = true;
+    private _containerId: ChartingLibraryWidgetOptions['container_id'] = 'tv_chart_container';
+    private _tvWidget: IChartingLibraryWidget | null = null;
+
+    
+    constructor(
+    ) {
 
     }
-    ngAfterViewInit() {
-        
-		// interval: 'D',
-		// containerId: 'tv_chart_container',
-		// // datafeedUrl: 'https://demo_feed.tradingview.com',
-		// datafeedUrl: `${BASE_URL_CORE}/tradingview`,
-		// libraryPath: '/charting_library/',
-		// chartsStorageUrl: 'https://saveload.tradingview.com',
-		// chartsStorageApiVersion: '1.1',
-		// clientId: 'tradingview.com',
-		// userId: 'public_user_id',
-		// timezone: "Asia/Ho_Chi_Minh",
-		// fullscreen: false,
-		// autosize: true,
-		// studiesOverrides: {}
-        new TradingView.widget(
-            {
-                "interval": "D",
-                "containerId": 'tv_chart_container',
-                "datafeedUrl": 'https://demo_feed.tradingview.com',
-                //"libraryPath": '/charting_library',
-                "chartsStorageUrl": 'https://saveload.tradingview.com',
-                "chartsStorageApiVersion": '1.1',
-                "clientId": 'tradingview.com',
-                "userId": 'public_user_id',
-                "timezone": "Asia/Ho_Chi_Minh",
-                "fullscreen": false,
-                "studiesOverrides": {},
 
-                "width": 980,
-                "height": 610,
-                "symbol": "BTCUSDT",
-                "theme": "Dark",
-                "style": "1",
-                "locale": "vi",
-                "toolbar_bg": "#f1f3f6",
-                "enable_publishing": false,
-                "withdateranges": true,
-                "range": "ytd",
-                "hide_side_toolbar": false,
-                "allow_symbol_change": true,
-                "show_popup_button": true,
-                "popup_width": "1000",
-                "popup_height": "650",
-                "no_referral_id": true,
-                "container_id": "tradingview_bac65"
-            }
-        );
+    @Input()
+    set symbol(symbol: ChartingLibraryWidgetOptions['symbol']) {
+        this._symbol = symbol || this._symbol;
+    }
+
+    @Input()
+    set interval(interval: ChartingLibraryWidgetOptions['interval']) {
+        this._interval = interval || this._interval;
+    }
+
+    @Input()
+    set datafeedUrl(datafeedUrl: string) {
+        this._datafeedUrl = datafeedUrl || this._datafeedUrl;
+    }
+
+    @Input()
+    set libraryPath(libraryPath: ChartingLibraryWidgetOptions['library_path']) {
+        this._libraryPath = libraryPath || this._libraryPath;
+    }
+
+    @Input()
+    set chartsStorageUrl(chartsStorageUrl: ChartingLibraryWidgetOptions['charts_storage_url']) {
+        this._chartsStorageUrl = chartsStorageUrl || this._chartsStorageUrl;
+    }
+
+    @Input()
+    set chartsStorageApiVersion(chartsStorageApiVersion: ChartingLibraryWidgetOptions['charts_storage_api_version']) {
+        this._chartsStorageApiVersion = chartsStorageApiVersion || this._chartsStorageApiVersion;
+    }
+
+    @Input()
+    set clientId(clientId: ChartingLibraryWidgetOptions['client_id']) {
+        this._clientId = clientId || this._clientId;
+    }
+
+    @Input()
+    set userId(userId: ChartingLibraryWidgetOptions['user_id']) {
+        this._userId = userId || this._userId;
+    }
+
+    @Input()
+    set fullscreen(fullscreen: ChartingLibraryWidgetOptions['fullscreen']) {
+        this._fullscreen = fullscreen || this._fullscreen;
+    }
+
+    @Input()
+    set autosize(autosize: ChartingLibraryWidgetOptions['autosize']) {
+        this._autosize = autosize || this._autosize;
+    }
+
+    @Input()
+    set containerId(containerId: ChartingLibraryWidgetOptions['container_id']) {
+        this._containerId = containerId || this._containerId;
+    }
+
+    ngOnInit() {
+        function getLanguageFromURL(): LanguageCode | null {
+            const regex = new RegExp('[\\?&]lang=([^&#]*)');
+            const results = regex.exec(location.search);
+
+            return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' ')) as LanguageCode;
+        }
+
+        const widgetOptions: ChartingLibraryWidgetOptions = {
+            symbol: this._symbol,
+            datafeed: new (window as any).Datafeeds.UDFCompatibleDatafeed(this._datafeedUrl),
+            interval: this._interval,
+            container_id: this._containerId,
+            library_path: this._libraryPath,
+            locale: getLanguageFromURL() || 'vi',
+            disabled_features: ['use_localstorage_for_settings'],
+            enabled_features: ['study_templates'],
+            charts_storage_url: this._chartsStorageUrl,
+            charts_storage_api_version: this._chartsStorageApiVersion,
+            client_id: this._clientId,
+            user_id: this._userId,
+            theme: 'Dark',
+            fullscreen: this._fullscreen,
+            autosize: this._autosize,
+        };
+
+        const tvWidget = new widget(widgetOptions);
+        this._tvWidget = tvWidget;
+
+        // tvWidget.onChartReady(() => {
+        //     tvWidget.headerReady().then(() => {
+        //         const button = tvWidget.createButton();
+        //         button.setAttribute('title', 'Click to show a notification popup');
+        //         button.classList.add('apply-common-tooltip');
+        //         button.addEventListener('click', () => tvWidget.showNoticeDialog({
+        //                 title: 'Notification',
+        //                 body: 'TradingView Charting Library API works correctly',
+        //                 callback: () => {
+        //                     console.log('Noticed!');
+        //                 },
+        //             }));
+        //         button.innerHTML = 'Check API';
+        //     });
+        // });
+    }
+
+    ngOnDestroy() {
+        if (this._tvWidget !== null) {
+            this._tvWidget.remove();
+            this._tvWidget = null;
+        }
     }
 }

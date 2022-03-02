a4154e436cff4a2615851c4b5177cd46b88e794b
Cuong Au <cuongah@vcsc.com.vn>
2022-03-02 15:08:01 +0700

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import * as $ from 'jquery';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
